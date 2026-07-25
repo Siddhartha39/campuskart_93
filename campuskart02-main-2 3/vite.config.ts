a29a79a2df11fe8bcfa8816248f380cdc876f947
Siddhartha39 <6387siddhartha@gmail.com>
2026-07-25 21:46:23 +0530

    Update vite.config.ts

--- campuskart02-main-2 3/vite.config.ts
+++ campuskart02-main-2 3/vite.config.ts
@@ -7,4 +7,14 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  server: {
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
+    },
+  },
+  preview: {
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
+    },
+  },
 });
